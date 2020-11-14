432ad914aab35875f19c7cd626688191d4cbdd83
irshadali97 <irshadali.cv97@gmail.com>
2020-11-14 12:37:36 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,223 @@
+console.log("Evaluating using if else if") ;
+let percentage=85
+ if (percentage<35) {
+     console.log("Fail") ;
+ }
+ else if ((percentage>=40) && (percentage<60)) {
+     console.log("Second Class");
+ }
+ else if ((percentage>=60) && (percentage<75)) {
+     console.log("First Class");
+ }
+ else if (percentage>=75) {
+     console.log("Distintion");
+ }
+ else {
+     console.log("Just pass");
+ }
+
+ console.log("Evaluating using switch")
+ var marks = 45;
+ switch (marks) {
+   case 35:
+       console.log("Just Pass")
+    break;
+    case 36:
+        console.log("Just Pass")
+     break;
+     case 37:
+        console.log("Just Pass")
+     break;
+     case 38:
+       console.log("Just Pass")
+    break;
+    case 39:
+       console.log("Just Pass")
+    break;
+    case 40:
+       console.log("Second Class")
+    break;
+    case 41:
+       console.log("Second Class")
+    break;
+    case 42:
+       console.log("Second Class")
+    break;
+    case 43:
+       console.log("Second Class")
+    break;
+    case 44:
+       console.log("Second Class")
+    break;
+    case 45:
+       console.log("Second Class")
+    break;
+    case 46:
+       console.log("Second Class")
+    break;
+    case 47:
+       console.log("Second Class")
+    break;
+    case 48:
+       console.log("Second Class")
+    break;
+    case 49:
+       console.log("Second Class")
+    break;
+    case 50:
+       console.log("Second Class")
+    break;
+    case 51:
+       console.log("Second Class")
+    break;
+    case 52:
+       console.log("Second Class")
+    break;
+    case 53:
+       console.log("Second Class")
+    break;
+    case 54:
+       console.log("Second Class")
+    break;
+    case 55:
+       console.log("Second Class")
+    break;
+    case 56:
+       console.log("Second Class")
+    break;
+    case 57:
+       console.log("Second Class")
+    break;
+    case 58:
+       console.log("Second Class")
+    break;
+    case 59:
+       console.log("Second Class")
+    break;
+    case 60:
+       console.log("First Class")
+    break;
+    case 61:
+       console.log("First Class")
+    break;
+    case 62:
+       console.log("First Class")
+    break;
+    case 63:
+       console.log("First Class")
+    break;
+    case 64:
+       console.log("First Class")
+    break;
+    case 65:
+       console.log("First Class")
+    break;
+    case 66:
+       console.log("First Class")
+    break;
+    case 67:
+       console.log("First Class")
+    break;
+    case 68:
+       console.log("First Class")
+    break;
+    case 69:
+       console.log("First Class")
+    break;
+    case 70:
+       console.log("First Class")
+    break;
+    case 71:
+       console.log("First Class")
+    break;
+    case 72:
+       console.log("First Class")
+    break;
+    case 73:
+       console.log("First Class")
+    break;
+    case 74:
+       console.log("First Class")
+    break;
+    case 75:
+       console.log("Distinction")
+    break;
+    case 76:
+       console.log("Distinction")
+    break;
+    case 77:
+       console.log("Distinction")
+    break;
+    case 78:
+       console.log("Distinction")
+    break;
+    case 79:
+       console.log("Distinction")
+    break;
+    case 80:
+       console.log("Distinction")
+    break;
+    case 81:
+       console.log("Distinction")
+    break;
+    case 82:
+       console.log("Distinction")
+    break;
+    case 83:
+       console.log("Distinction")
+    break;
+    case 84:
+       console.log("Distinction")
+    break;
+    case 85:
+       console.log("Distinction")
+    break;
+    case 86:
+       console.log("Distinction")
+    break;
+    case 87:
+       console.log("Distinction")
+    break;
+    case 88:
+       console.log("Distinction")
+    break;
+    case 89:
+       console.log("Distinction")
+    break;
+    case 90:
+       console.log("Distinction")
+    break;
+    case 91:
+       console.log("Distinction")
+    break;
+    case 92:
+       console.log("Distinction")
+    break;
+    case 93:
+       console.log("Distinction")
+    break;
+    case 94:
+       console.log("Distinction")
+    break;
+    case 95:
+       console.log("Distinction")
+    break;
+    case 96:
+       console.log("Distinction")
+    break;
+    case 97:
+       console.log("Distinction")
+    break;
+    case 98:
+       console.log("Distinction")
+    break;
+    case 99:
+       console.log("Distinction")
+    break;
+    case 100:
+       console.log("Distinction")
+    break;
+    default:
+        console.log("Fail")
+ }
+ 
